e82595893fe43aae1785ad3fd416789228e7df05
Marcelo Poma <mpoma@yahoo.com>
2016-05-16 13:35:12 -0300

    cambio getTarget

--- src/MikrotikAPI/Commands/Queues/Queues.php
+++ src/MikrotikAPI/Commands/Queues/Queues.php
@@ -60,6 +60,28 @@ class Queues {
         }
     }
 
+
+    /**
+     * This method is used to display one target simple queue
+     * @return type array
+     * 
+     */
+    public function getTarget($target) {
+        $sentence = new SentenceUtil();
+        $sentence->fromCommand("/queue/simple/getall");
+        $sentence->where("target", "=", $target);
+        $this->talker->send($sentence);
+        $rs = $this->talker->getResult();
+        $i = 0;
+        if ($i < $rs->size()) {
+            return $rs->getResultArray();
+        } else {
+            return null;
+        }
+    }
+
+
+
     /**
      * This method is used to activate simple queue by id
      * @param type $id is not an array
@@ -128,7 +150,7 @@ class Queues {
      * @return type array
      * 
      */
-    public function detail_queue($id) {
+    public function detail_address($id) {
         $sentence = new SentenceUtil();
         $sentence->fromCommand("/queue/simple/print");
         $sentence->where(".id", "=", $id);
